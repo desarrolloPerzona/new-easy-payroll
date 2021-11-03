9090a0dce8ad6a19e04eac3a5d43c16ec15447ba
GIT DSSD - NUEVECUBICA <git-dssd-nueve3@nuevecubica.net>
2021-11-03 12:38:53 -0600

    031121 chore: fixing on tenant assets  12:38

--- resources/views/include/common/head/vendor-css.blade.php
+++ resources/views/include/common/head/vendor-css.blade.php
@@ -1,15 +1,15 @@
 {{--BOOTSTRAP--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/bootstrap/css/bootstrap.css')}}">
 {{--TIPPY--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/tippy/tippy.js/themes/light.css')}}">
-<link rel="stylesheet" href="{{asset('assets/vendor/tippy/tippy.js/themes/translucent.css')}}">
-<link rel="stylesheet" href="{{asset('assets/vendor/tippy/tippy.js/themes/material.css')}}">
-<link rel="stylesheet" href="{{asset('assets/vendor/tippy/tippy.js/animations/scale.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/tippy/tippy.js/themes/light.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/tippy/tippy.js/themes/translucent.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/tippy/tippy.js/themes/material.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/tippy/tippy.js/animations/scale.css')}}">
 {{--FONTAWESOME--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/fontawesome/css/all.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/fontawesome/css/all.css')}}">
 {{--ANIMATE CSS--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/animate/animate.css')}}">
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/animate/animate.css')}}">
 {{--SELECT2--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/select2/css/select2.css')}}"  />
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/select2/css/select2.css')}}"  />
 {{--PIKADAY--}}
-<link rel="stylesheet" href="{{asset('assets/vendor/pikaday/css/pikaday.css')}}"  />
+<link rel="stylesheet" href="{{asset('tenancy/assets/vendor/pikaday/css/pikaday.css')}}"  />
--- resources/views/include/common/scripts/vendor-scripts.blade.php
+++ resources/views/include/common/scripts/vendor-scripts.blade.php
@@ -1,19 +1,19 @@
 {{--JQUERY--}}
-<script src="{{asset('assets/vendor/jquery/jquery.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/jquery/jquery.js')}}"></script>
 {{--BOOTSTRAP--}}
-<script src="{{asset('assets/vendor/bootstrap/js/bootstrap.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/bootstrap/js/bootstrap.js')}}"></script>
 {{--POPPER@2--}}
-<script src="{{asset('assets/vendor/popper-js2/umd/popper.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/popper-js2/umd/popper.js')}}"></script>
 {{--FONTAWESOME--}}
-<script src="{{asset('assets/vendor/fontawesome/js/all.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/fontawesome/js/all.js')}}"></script>
 {{--TIPPY--}}
-<script src="{{asset('assets/vendor/tippy/tippy.js/dist/tippy-bundle.umd.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/tippy/tippy.js/dist/tippy-bundle.umd.js')}}"></script>
 {{--SELECT2--}}
-<script src="{{asset('assets/vendor/select2/js/select2.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/select2/js/select2.js')}}"></script>
 {{--CKEDITOR5--}}
-<script src="{{asset('assets/vendor/ckeditor5/ckeditor.js')}}"></script>
-<script src="{{asset('assets/vendor/ckeditor5/translations/es.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/ckeditor5/ckeditor.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/ckeditor5/translations/es.js')}}"></script>
 {{--PIKADAY--}}
-<script src="{{asset('assets/vendor/pikaday/pikaday.js')}}"></script>
+<script src="{{asset('tenancy/assets/vendor/pikaday/pikaday.js')}}"></script>
 {{--STRIPE--}}
 <script src="https://js.stripe.com/v3/"></script>

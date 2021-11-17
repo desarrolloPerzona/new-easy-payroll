f357aa55913cc43ed288ba61c200cc96bb249054
GIT DSSD - NUEVECUBICA <git-dssd-nueve3@nuevecubica.net>
2021-11-17 13:49:57 -0600

    041121 chore: payroll 1:28

--- database/migrations/tenant/2021_11_16_123157_create_employer_register_table.php
+++ database/migrations/tenant/2021_11_16_123157_create_employer_register_table.php
@@ -13,9 +13,23 @@ class CreateEmployerRegisterTable extends Migration
      */
     public function up()
     {
+
         Schema::create('employer_register', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('business_branch_id');
+            $table->string('name');
+            $table->string('risk_premium')->nullable();
+            $table->string('imss_sub_delegation_key')->nullable();
+            $table->boolean('use_imss_cert')->default(false);
+            $table->string('imss_cert_user')->nullable();
+            $table->string('imss_cert_password')->nullable();
+            $table->string('imss_cert_file')->nullable();
+            $table->boolean('use_imss_cert_fiel')->default(false);
+            $table->string('imss_cert_fiel_file')->nullable();
+            $table->string('imss_cert_fiel_private_key')->nullable();
+            $table->string('imss_cert_fiel_password')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

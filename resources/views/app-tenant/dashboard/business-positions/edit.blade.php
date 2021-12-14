f8c9adafc32e58a90f79e586ffb889d843fb6b35
cradyDSSD <39503543+cradyDSSD@users.noreply.github.com>
2021-12-14 10:53:53 -0600

    Crady 13/12/2021 Business Positions

--- resources/views/app-tenant/dashboard/business-positions/edit.blade.php
+++ resources/views/app-tenant/dashboard/business-positions/edit.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto">
         <h2 class="bg-blueSteel py-2 px-3 mb-2 rounded">
             <i class="nav-icon fas fa-sitemap mr-2"></i>
-            <span style="display: inline-flex;">Editar Director MKT</span>
+            <span style="display: inline-flex;">{{__('Business position edit')}}</span>
         </h2>
 
         {{--Seat--}}

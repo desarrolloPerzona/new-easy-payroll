a2aa8b1232e8be6e9b7936c9a047b3bdbb2e28e7
EduardoGRQ <eduardo.grquinonez@gmail.com>
2022-01-17 23:27:01 -0600

    Moving imms patronal register component.

---
+++ app/Http/Livewire/Tenant/ImmsPatronalRegister/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Tenant\ImmsPatronalRegister;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.tenant.imms-patronal-register.create');
+    }
+}
--- resources/views/app-tenant/dashboard/imss-employer-register/create.blade.php
+++ resources/views/app-tenant/dashboard/imss-employer-register/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-tenant>
     <div class="container mx-auto">
         <livewire:components.breadcrumb :parent="'IMSS Employer Registers'" :children="[]" item-id="{{route('imss-employer-registers.index')}}" :icon="'fak fa-empresa-perzona mr-2'"/>
-        <livewire:components.crud.employer-register.create/>
+        <livewire:tenant.imms-patronal-register.create/>
     </div>
 </x-app-tenant>

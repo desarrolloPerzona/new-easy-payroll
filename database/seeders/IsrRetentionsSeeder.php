43ec766b3c661419ac8b99fdedf49ed463a9fb38
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-09 17:46:07 -0600

    Monthly retentions values added to new table

--- database/seeders/IsrRetentionsSeeder.php
+++ database/seeders/IsrRetentionsSeeder.php
@@ -80,6 +80,22 @@ class IsrRetentionsSeeder extends Seeder
 
         );
 
+        // filling array with Monthly table data
+        $monthlyArray = array(
+            array(0.01, 644.58, 0.00, 1.92),
+            array(644.59, 5470.92, 12.38, 6.40),
+            array(5470.93, 9614.66, 321.26, 10.88),
+            array(9614.67, 11176.62, 772.10, 16.00),
+            array(11176.63, 13381.47, 1022.01, 17.92),
+            array(13381.48, 26988.50, 1417.12, 21.36),
+            array(26988.51, 42537.58, 4323.58, 23.52),
+            array(42537.59, 81211.25, 7980.73, 30.00),
+            array(81211.26, 108281.67, 19582.83, 32.00),
+            array(108281.68, 324845.01, 28245.36, 34.00),
+            array(324845.02, 999999, 101876.90, 35.00),
+
+        );
+
         //        2021
         // Inserting data for each element of dailyArray
         foreach ($dailyArray as $element) {
@@ -132,7 +148,7 @@ class IsrRetentionsSeeder extends Seeder
         }
 
         //        2022
-        // Inserting data for each element of weeklyArray
+        // Inserting data for each element of biweeklyArray
         foreach ($biweeklyArray as $element) {
             DB::table('isr_retentions')->insert([
                 [
@@ -142,6 +158,23 @@ class IsrRetentionsSeeder extends Seeder
                     'percentage_excess_to_lower_limit' => $element[3],
                     'year' => 2022,
                     'isr_retention_type' => 'biweekly',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            ]);
+        }
+
+        //        2022
+        // Inserting data for each element of monthlyArray
+        foreach ($monthlyArray as $element) {
+            DB::table('isr_retentions')->insert([
+                [
+                    'lower_limit' => $element[0],
+                    'upper_limit' => $element[1],
+                    'fixed_feed' => $element[2],
+                    'percentage_excess_to_lower_limit' => $element[3],
+                    'year' => 2022,
+                    'isr_retention_type' => 'monthly',
                     'created_at' => now(),
                     'updated_at' => now(),
                 ]

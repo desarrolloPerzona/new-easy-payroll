53f7c128306821d3d80b8fbb5e4ab9851c9ebc29
DSSD-NUEVE <git-dssd-nueve3@nuevecubica.net>
2022-02-09 11:40:16 -0600

    090222 Alex chore: isn payroll taxes, daily allowances tables.

---
+++ database/migrations/2022_02_09_113202_create_isn_payroll_taxes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIsnPayrollTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('isn_payroll_taxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('state');
+            $table->decimal('lower_limit');
+            $table->decimal('upper_limit');
+            $table->decimal('fixed_fee');
+            $table->decimal('lower_limit_surplus_percentage');
+            $table->smallInteger('year');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('isn_payroll_taxes');
+    }
+}

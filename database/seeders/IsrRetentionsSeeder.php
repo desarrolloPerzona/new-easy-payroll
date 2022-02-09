92b80e7d4df6ba159b4336b2646c5d19ca4dbf19
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-09 17:33:39 -0600

    Weekly retentions values added to new table

--- database/seeders/IsrRetentionsSeeder.php
+++ database/seeders/IsrRetentionsSeeder.php
@@ -32,6 +32,22 @@ class IsrRetentionsSeeder extends Seeder
 
         );
 
+        //        2021 and 2022 values
+        // filling array with Weekly table data
+        $weeklyArray = array(
+            array(0.01, 148.4, 0.00, 1.92),
+            array(148.41, 1259.72, 2.87, 6.40),
+            array(1259.73, 2213.89, 73.99, 10.88),
+            array(2213.90, 2573.55, 177.8, 16.00),
+            array(2573.56, 3081.26, 235.34, 17.92),
+            array(3081.27, 6214.46, 326.34, 21.36),
+            array(6214.47, 9794.82, 995.54, 23.52),
+            array(9794.83, 18699.94, 1837.64, 30.00),
+            array(18699.95, 24933.30, 4509.19, 32.00),
+            array(24933.31, 74799.83, 6503.84, 34.00),
+            array(74799.84, 999999, 23458.47, 35.00),
+        );
+
         //        2021
         // Inserting data for each element of dailyArray
         foreach ($dailyArray as $element) {
@@ -43,6 +59,23 @@ class IsrRetentionsSeeder extends Seeder
                     'percentage_excess_to_lower_limit' => $element[3],
                     'year' => 2022,
                     'isr_retention_type' => 'daily',
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]
+            ]);
+        }
+
+        //        2022
+        // Inserting data for each element of weeklyArray
+        foreach ($weeklyArray as $element) {
+            DB::table('isr_retentions')->insert([
+                [
+                    'lower_limit' => $element[0],
+                    'upper_limit' => $element[1],
+                    'fixed_feed' => $element[2],
+                    'percentage_excess_to_lower_limit' => $element[3],
+                    'year' => 2022,
+                    'isr_retention_type' => 'weekly',
                     'created_at' => now(),
                     'updated_at' => now()
                 ]

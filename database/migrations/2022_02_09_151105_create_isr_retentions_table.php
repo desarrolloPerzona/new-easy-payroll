ea8895c2bf59047a8c7183d0eb1c6774fb605bc0
DSSD-NUEVE <git-dssd-nueve3@nuevecubica.net>
2022-02-09 15:20:11 -0600

    090222 Alex chore: isn payroll taxes, daily allowances tables.

--- database/migrations/2022_02_09_151105_create_isr_retentions_table.php
+++ database/migrations/2022_02_09_151105_create_isr_retentions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateIsrDailyRetentionsTable extends Migration
+class CreateIsrRetentionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateIsrDailyRetentionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('isr_daily_retentions', function (Blueprint $table) {
+        Schema::create('isr_retentions', function (Blueprint $table) {
             $table->id();
             $table->decimal('lower_limit');
             $table->decimal('upper_limit');
             $table->decimal('fixed_feed');
             $table->decimal('percentage_excess_to_lower_limit');
             $table->smallInteger('year');
-
+            $table->string('isr_retention_type');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateIsrDailyRetentionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('isr_daily_retentions');
+        Schema::dropIfExists('isr_retentions');
     }
 }

ad39f3150a3607c0cebd9e1e1725a0bdee5401b2
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-10 14:00:41 -0600

    Show user profile refactored to change between tabs and show its tables

--- resources/views/app-tenant/dashboard/employees-list/show.blade.php
+++ resources/views/app-tenant/dashboard/employees-list/show.blade.php
@@ -1,5 +1,8 @@
 <x-app-tenant>
+
     <div class="container">
+
+{{--        Header User profile--}}
         <h2 class="bg-blueSteel py-2 px-3 mb-2 rounded">
             <i class="fak fa-admin-perzona mr-2"></i>
             <span style="display: inline-flex;">Juan Perez</span>
@@ -10,7 +13,7 @@
                 <img class="px-5 m-auto" src="/assets/images/avatar/user.jpg" alt="">
             </div>
             <div class="flex-1 w-3/12">
-              <h2 class="mb-4">Juan Perez</h2>
+                <h2 class="mb-4">Juan Perez</h2>
                 <div class="mb-2"><i class="fa-solid fa-square-dollar fa-xl"></i> $ 35,000</div>
                 <div class="mb-2"><i class="fa-solid fa-envelope fa-xl"></i> juan.perez@espresa.com</div>
                 <div class="mb-2"><i class="fa-regular fa-envelope fa-xl"></i> juan.perez@personal.com</div>
@@ -30,47 +33,56 @@
             </div>
         </div>
 
+{{--        Tabs Menu--}}
         <div id="collapse-retentions" x-data="data()" class="px-2">
+
+            {{--            Header Tabs buttons--}}
             <ul class="nav nav-tabs w-full mb-4" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active tab-retention cursor-pointer bg-primary text-white" id="daily-button" x-on:click="changeTab('personal')">
+                    <a class="nav-link active tab-retention cursor-pointer bg-primary text-white" id="personal-button"
+                       x-on:click="changeTab('personal')">
                         <i class="now-ui-icons objects_umbrella-13"></i> Personal
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="weekly-button" x-on:click="changeTab('employee')">
+                    <a class="nav-link tab-retention cursor-pointer" id="employee-button"
+                       x-on:click="changeTab('employee')">
                         <i class="now-ui-icons shopping_cart-simple"></i> Empleo
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="ten-days-button" x-on:click="changeTab('payment')">
+                    <a class="nav-link tab-retention cursor-pointer" id="payment-button"
+                       x-on:click="changeTab('payment')">
                         <i class="now-ui-icons shopping_shop"></i> Pago
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="ten-days-button" x-on:click="changeTab('events')">
+                    <a class="nav-link tab-retention cursor-pointer" id="events-button"
+                       x-on:click="changeTab('events')">
                         <i class="now-ui-icons shopping_shop"></i> Eventos
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="biweekly-button" x-on:click="changeTab('documents')">
+                    <a class="nav-link tab-retention cursor-pointer" id="documents-button"
+                       x-on:click="changeTab('documents')">
                         <i class="now-ui-icons ui-2_settings-90"></i> Documentos
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="monthly-button" x-on:click="changeTab('kardex')">
+                    <a class="nav-link tab-retention cursor-pointer" id="kardex-button"
+                       x-on:click="changeTab('kardex')">
                         <i class="now-ui-icons ui-2_settings-90"></i> Kardex
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link tab-retention cursor-pointer" id="monthly-button" x-on:click="changeTab('notes')">
+                    <a class="nav-link tab-retention cursor-pointer" id="notes-button" x-on:click="changeTab('notes')">
                         <i class="now-ui-icons ui-2_settings-90"></i> Notas
                     </a>
                 </li>
             </ul>
 
             {{--                Personal--}}
-            <div id="personal" class="retentions-table">
+            <div id="personal-table" class="retentions-table">
 
                 <table class="table table-striped">
                     <thead>
@@ -82,19 +94,19 @@
                     </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                    <tr>
+                        <td>Personal</td>
+                        <td>Personal</td>
+                        <td>Personal</td>
+                        <td>Personal</td>
+                    </tr>
                     </tbody>
                 </table>
 
             </div>
 
             {{--                Empleo--}}
-            <div id="employee" class="retentions-table">
+            <div id="employee-table" class="retentions-table d-none">
 
                 <table class="table table-striped">
                     <thead>
@@ -107,49 +119,150 @@
                     </thead>
                     <tbody>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>Empleo</td>
+                        <td>Empleo</td>
+                        <td>Empleo</td>
+                        <td>Empleo</td>
                     </tr>
                     </tbody>
                 </table>
 
             </div>
 
+            {{--            Pago--}}
+            <div id="payment-table" class="retentions-table d-none">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Límite inferior')}}</th>
+                        <th scope="col">{{__('Límite superior')}}</th>
+                        <th scope="col">{{__('Cuota fija')}}</th>
+                        <th scope="col">{{__('Excedentes %')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Pago</td>
+                        <td>Pago</td>
+                        <td>Pago</td>
+                        <td>Pago</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
 
+            {{--            Eventos--}}
+            <div id="events-table" class="retentions-table d-none">
+
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Límite inferior')}}</th>
+                        <th scope="col">{{__('Límite superior')}}</th>
+                        <th scope="col">{{__('Cuota fija')}}</th>
+                        <th scope="col">{{__('Excedentes %')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Eventos</td>
+                        <td>Eventos</td>
+                        <td>Eventos</td>
+                        <td>Eventos</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+            {{--            Documents--}}
+            <div id="documents-table" class="retentions-table d-none">
+
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Límite inferior')}}</th>
+                        <th scope="col">{{__('Límite superior')}}</th>
+                        <th scope="col">{{__('Cuota fija')}}</th>
+                        <th scope="col">{{__('Excedentes %')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Documentos</td>
+                        <td>Documentos</td>
+                        <td>Documentos</td>
+                        <td>Documentos</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+            {{--            Kardex--}}
+            <div id="kardex-table" class="retentions-table d-none">
+
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Límite inferior')}}</th>
+                        <th scope="col">{{__('Límite superior')}}</th>
+                        <th scope="col">{{__('Cuota fija')}}</th>
+                        <th scope="col">{{__('Excedentes %')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Kardex</td>
+                        <td>Kardex</td>
+                        <td>Kardex</td>
+                        <td>Kardex</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+            {{--            Kardex--}}
+            <div id="notes-table" class="retentions-table d-none">
+
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">{{__('Límite inferior')}}</th>
+                        <th scope="col">{{__('Límite superior')}}</th>
+                        <th scope="col">{{__('Cuota fija')}}</th>
+                        <th scope="col">{{__('Excedentes %')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Notas</td>
+                        <td>Notas</td>
+                        <td>Notas</td>
+                        <td>Notas</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
 
         </div>
 
     </div>
     @push('inline_scripts')
         <script>
-
-            // Function to collapse tables with effect and changeing arrow icon
-            const arrowClick = (name) => {
-                $("#collapse-" + name).slideToggle(function() {
-                    let arrowButton = document.getElementById(`${name}-icon`)
-                    if (arrowButton.classList.contains('fa-chevron-circle-down')){
-                        arrowButton.classList.remove('fa-chevron-circle-down')
-                        arrowButton.classList.add('fa-chevron-circle-up')
-                    } else{
-                        arrowButton.classList.remove('fa-chevron-circle-up')
-                        arrowButton.classList.add('fa-chevron-circle-down')
-                    }
-                });
-            }
-
             // Alpine function to change between tabs
-            function data(){
-                return{
-                    changeTab(button){
+            function data() {
+                return {
+                    changeTab(button) {
                         let retentionsButton = document.getElementById(`${button}-button`);
                         let retentionTabs = document.querySelectorAll('.tab-retention')
                         let allRetentionTables = document.querySelectorAll('.retentions-table')
                         let retentionTable = document.getElementById(`${button}-table`)
 
                         // Change all active classes to the othew tabs and added only the clicked button, active class.
-                        if(!retentionsButton.classList.contains('active')){
+                        if (!retentionsButton.classList.contains('active')) {
                             retentionTabs.forEach(item => {
                                 item.classList.remove('active', 'bg-primary', 'text-white')
                             })

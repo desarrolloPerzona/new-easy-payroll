debfe0bb89d8122e35202db96893fd37b312847e
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-09 18:44:54 -0600

    Weekly and ten days Subsidies added to Subsidies table

--- database/seeders/IsrSubsidiesSeeder.php
+++ database/seeders/IsrSubsidiesSeeder.php
@@ -15,7 +15,7 @@ class IsrSubsidiesSeeder extends Seeder
     public function run()
     {
         //        2021 && 2022 values
-        // filling array with Monthly table data
+        // filling array with daily table data
         $dailyArray = array(
             array(0.01, 58.19, 13.39),
             array(58.2, 87.28, 13.38),
@@ -30,8 +30,38 @@ class IsrSubsidiesSeeder extends Seeder
             array(242.85, 999999, 0),
         );
 
+        //        2021 && 2022 values
+        // filling array with Weekly table data
+        $weeklyArray = array(
+            array(0.01, 407.33, 93.73),
+            array(407.34, 610.96, 93.66),
+            array(610.97, 799.68, 93.66),
+            array(799.69, 814.66, 90.44),
+            array(814.67, 1023.75, 88.06),
+            array(1023.76, 1086.19, 81.55),
+            array(1086.20, 1228.57, 74.83),
+            array(1228.58, 1433.32, 67.83),
+            array(1433.33, 1638.07, 58.38),
+            array(1638.08, 1699.88, 50.12),
+            array(1699.89, 999999, 0),
+        );
+
+        $tenDaysArray = array(
+            array(0.01, 581.9, 133.9),
+            array(581.91, 872.8, 133.8),
+            array(872.81, 1142.4, 133.8),
+            array(1142.41, 1163.8, 129.2),
+            array(1163.81, 1462.5, 125.8),
+            array(1462.51, 1551.7, 116.5),
+            array(1551.71, 1755.1, 106.9),
+            array(1755.11, 2047.6, 96.9),
+            array(2047.61, 2340.1, 83.4),
+            array(2340.11, 2428.4, 71.6),
+            array(2428.41, 999999, 0),
+        );
+
         //        2021
-        // Inserting data for each element of dailyArray
+        // Inserting data for each element of the Array
         foreach ($dailyArray as $element) {
             DB::table('isr_subsidies')->insert([
                 [
@@ -40,6 +70,38 @@ class IsrSubsidiesSeeder extends Seeder
                     'employment_subsidy' => $element[2],
                     'year' => 2022,
                     'subsidy_type' => 'daily',
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]
+            ]);
+        }
+
+        //        2022
+        // Inserting data for each element of the Array
+        foreach ($weeklyArray as $element) {
+            DB::table('isr_subsidies')->insert([
+                [
+                    'income_from' => $element[0],
+                    'income_up_to' => $element[1],
+                    'employment_subsidy' => $element[2],
+                    'year' => 2022,
+                    'subsidy_type' => 'weekly',
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]
+            ]);
+        }
+
+        //        2022
+        // Inserting data for each element of the Array
+        foreach ($tenDaysArray as $element) {
+            DB::table('isr_subsidies')->insert([
+                [
+                    'income_from' => $element[0],
+                    'income_up_to' => $element[1],
+                    'employment_subsidy' => $element[2],
+                    'year' => 2022,
+                    'subsidy_type' => 'ten days',
                     'created_at' => now(),
                     'updated_at' => now()
                 ]

2c29de33652ef80c2449d4a7010198b7fe8b6949
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-11 10:54:07 -0600

    Corrections

--- app/Http/Controllers/Tenant/TableValueController.php
+++ app/Http/Controllers/Tenant/TableValueController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Tenant;
 
 use App\Http\Controllers\Controller;
-use App\Models\IsrDailyRetention;
 use App\Models\Tenant\TableValue;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;

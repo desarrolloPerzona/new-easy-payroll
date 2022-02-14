104fc722ebe4cb78c1b41e23d51e538facca9659
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-14 11:35:57 -0600

    Functiohn to change between tabs added to subsidies table

--- resources/views/app-tenant/dashboard/table-value/index.blade.php
+++ resources/views/app-tenant/dashboard/table-value/index.blade.php
@@ -258,17 +258,173 @@
         {{--        Periodic Subsidies Table--}}
         <div class="card bg-white shadow-sm rounded p-4 max-w-6xl my-3 mx-auto dark:bg-dark dark:text-white">
             <div class="flex">
-                <div class="flex-1"><h2 class="pb-3">Tablas periódicas a subsidio</h2></div>
+                <div class="flex-1"><h2 class="pb-3">{{__('Tablas periódicas a subsidio')}}</h2></div>
                 <div class="flex-2" onclick="arrowClick('subsidies')">
                     <div id="subsidies-button">
                         <i id="subsidies-icon" class="cursor-pointer fas fa-chevron-circle-down"></i>
                     </div>
                 </div>
             </div>
-            <div id="collapse-subsidies">
+{{--            Subsidies Tab Tables--}}
+            <div id="collapse-subsidies" x-data="subsidiesData()">
+                <ul class="nav nav-tabs w-full mb-4" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active tab-subsidies cursor-pointer bg-primary text-white" id="dailySubsidies-button"
+                           x-on:click="changeTab('dailySubsidies')">
+                            <i class="now-ui-icons objects_umbrella-13"></i> Diaria
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tab-subsidies cursor-pointer" id="weeklySubsidies-button"
+                           x-on:click="changeTab('weeklySubsidies')">
+                            <i class="now-ui-icons shopping_cart-simple"></i> Semanal
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tab-subsidies cursor-pointer" id="ten-daysSubsidies-button"
+                           x-on:click="changeTab('ten-daysSubsidies')">
+                            <i class="now-ui-icons shopping_shop"></i> Decenal
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tab-subsidies cursor-pointer" id="biweeklySubsidies-button"
+                           x-on:click="changeTab('biweeklySubsidies')">
+                            <i class="now-ui-icons ui-2_settings-90"></i> Quincenal
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link tab-subsidies cursor-pointer" id="monthlySubsidies-button"
+                           x-on:click="changeTab('monthlySubsidies')">
+                            <i class="now-ui-icons ui-2_settings-90"></i> Mensual
+                        </a>
+                    </li>
+                </ul>
+
+                {{--                Daily table--}}
+                <div id="dailySubsidies-table" class="subsidies-table">
+
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Límite inferior')}}</th>
+                            <th scope="col">{{__('Límite superior')}}</th>
+                            <th scope="col">{{__('Cuota fija')}}</th>
+                            <th scope="col">{{__('Excedentes %')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($dailyRetentions as $dailyValues)
+                            <tr>
+                                <td>{{$dailyValues->lower_limit}}</td>
+                                <td>{{$dailyValues->upper_limit}}</td>
+                                <td>{{$dailyValues->fixed_feed}}</td>
+                                <td>{{$dailyValues->percentage_excess_to_lower_limit}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
+
+                {{--                Weekly table--}}
+                <div id="weeklySubsidies-table" class="d-none subsidies-table">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Límite inferior')}}</th>
+                            <th scope="col">{{__('Límite superior')}}</th>
+                            <th scope="col">{{__('Cuota fija')}}</th>
+                            <th scope="col">{{__('Excedentes %')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($weeklyRetentions as $weeklyValues)
+                            <tr>
+                                <td>{{$weeklyValues->lower_limit}}</td>
+                                <td>{{$weeklyValues->upper_limit}}</td>
+                                <td>{{$weeklyValues->fixed_feed}}</td>
+                                <td>{{$weeklyValues->percentage_excess_to_lower_limit}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                {{--                Ten days table--}}
+                <div id="ten-daysSubsidies-table" class="d-none subsidies-table">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Límite inferior')}}</th>
+                            <th scope="col">{{__('Límite superior')}}</th>
+                            <th scope="col">{{__('Cuota fija')}}</th>
+                            <th scope="col">{{__('Excedentes %')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($tenDaysRetentions as $tenDaysValues)
+                            <tr>
+                                <td>{{$tenDaysValues->lower_limit}}</td>
+                                <td>{{$tenDaysValues->upper_limit}}</td>
+                                <td>{{$tenDaysValues->fixed_feed}}</td>
+                                <td>{{$tenDaysValues->percentage_excess_to_lower_limit}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                {{--                Biweekly table--}}
+                <div id="biweeklySubsidies-table" class="d-none subsidies-table">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Límite inferior')}}</th>
+                            <th scope="col">{{__('Límite superior')}}</th>
+                            <th scope="col">{{__('Cuota fija')}}</th>
+                            <th scope="col">{{__('Excedentes %')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($biweeklyRetentions as $biweeklyValues)
+                            <tr>
+                                <td>{{$biweeklyValues->lower_limit}}</td>
+                                <td>{{$biweeklyValues->upper_limit}}</td>
+                                <td>{{$biweeklyValues->fixed_feed}}</td>
+                                <td>{{$biweeklyValues->percentage_excess_to_lower_limit}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                {{--                Monthly table--}}
+                <div id="monthlySubsidies-table" class="d-none subsidies-table">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{__('Límite inferior')}}</th>
+                            <th scope="col">{{__('Límite superior')}}</th>
+                            <th scope="col">{{__('Cuota fija')}}</th>
+                            <th scope="col">{{__('Excedentes %')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($monthlyRetentions as $monthlyValues)
+                            <tr>
+                                <td>{{$monthlyValues->lower_limit}}</td>
+                                <td>{{$monthlyValues->upper_limit}}</td>
+                                <td>{{$monthlyValues->fixed_feed}}</td>
+                                <td>{{$monthlyValues->percentage_excess_to_lower_limit}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
 
             </div>
         </div>
+
 
         {{--        Taxes tables--}}
         <div class="card bg-white shadow-sm rounded p-4 max-w-6xl my-3 mx-auto dark:bg-dark dark:text-white">
@@ -298,14 +454,14 @@
                 });
             }
 
-            // Alpine function to change between tabs
+            // Alpine function to change between tabs in Retentions ISR Section
             function data() {
                 return {
                     changeTab(button) {
                         let retentionsButton = document.getElementById(`${button}-button`);
                         let retentionTabs = document.querySelectorAll('.tab-retention')
                         let allRetentionTables = document.querySelectorAll('.retentions-table')
-                        let retentionTable = document.getElementById(`${button}-table`)
+                        let retentionTable = document.getElementById(`${button}Subsidy-table`)
 
                         // Change all active classes to the othew tabs and added only the clicked button, active class.
                         if (!retentionsButton.classList.contains('active')) {
@@ -317,6 +473,30 @@
                             })
                             retentionsButton.classList.add('active', 'bg-primary', 'text-white')
                             retentionTable.classList.remove('d-none')
+                        }
+                    }
+                }
+            }
+
+            // Alpine function to change between tabs in Subsidies Section
+            function subsidiesData() {
+                return {
+                    changeTab(button) {
+                        let subsidiesButton = document.getElementById(`${button}-button`);
+                        let subsidiesTabs = document.querySelectorAll('.tab-subsidies')
+                        let allSubsidiesTables = document.querySelectorAll('.subsidies-table')
+                        let subsidyTable = document.getElementById(`${button}-table`)
+
+                        // Change all active classes to the othew tabs and added only the clicked button, active class.
+                        if (!subsidiesButton.classList.contains('active')) {
+                            subsidiesTabs.forEach(item => {
+                                item.classList.remove('active', 'bg-primary', 'text-white')
+                            })
+                            allSubsidiesTables.forEach(item => {
+                                item.classList.add('d-none')
+                            })
+                            subsidiesButton.classList.add('active', 'bg-primary', 'text-white')
+                            subsidyTable.classList.remove('d-none')
                         }
                     }
                 }

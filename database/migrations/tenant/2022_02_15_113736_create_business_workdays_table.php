625db26196693c88c6ee68ee00ff9f6023a888f2
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-15 12:25:41 -0600

    Working days migration completed

--- database/migrations/tenant/2022_02_15_113736_create_business_workdays_table.php
+++ database/migrations/tenant/2022_02_15_113736_create_business_workdays_table.php
@@ -17,9 +17,34 @@ class CreateBusinessWorkdaysTable extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('workday_type');
+
             $table->boolean('monday')->default(false);
             $table->time('monday_from');
             $table->time('monday_to');
+
+            $table->boolean('tuesday')->default(false);
+            $table->time('tuesday_from');
+            $table->time('tuesday_to');
+
+            $table->boolean('wednesday')->default(false);
+            $table->time('wednesday_from');
+            $table->time('wednesday_to');
+
+            $table->boolean('thursday')->default(false);
+            $table->time('thursday_from');
+            $table->time('thursday_to');
+
+            $table->boolean('friday')->default(false);
+            $table->time('friday_from');
+            $table->time('friday_to');
+
+            $table->boolean('saturday')->default(false);
+            $table->time('saturday_from');
+            $table->time('saturday_to');
+
+            $table->boolean('sunday')->default(false);
+            $table->time('sunday_from');
+            $table->time('sunday_to');
 
             $table->boolean('meal_time')->default(false);
             $table->time('meal_time_from');

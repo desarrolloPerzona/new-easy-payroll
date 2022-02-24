73ea6a65eba10efec178d9f702061ca44755a76b
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-24 12:16:25 -0600

    Messages corrections

--- resources/views/components/forms/alert-message.blade.php
+++ resources/views/components/forms/alert-message.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- delete, edit or update message--}}
     @if (session($message))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <div class="alert alert-success alert-dismissible fade show max-w-6xl mx-auto" role="alert">
             {{ __(session($message)) }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
--- resources/views/components/forms/error-validation-message.blade.php
+++ resources/views/components/forms/error-validation-message.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if ($errors->any())
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div class="alert alert-warning alert-dismissible fade show max-w-6xl mx-auto" role="alert">
             @foreach ($errors->all() as $error)
                 {{$error}}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>

91a8b0745dd6a9f299e65dcfd28c51194b912df0
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-28 14:00:10 -0600

    Refactoring banks create table

--- resources/views/app-tenant/dashboard/banks/create.blade.php
+++ resources/views/app-tenant/dashboard/banks/create.blade.php
@@ -12,8 +12,10 @@
 
                 <div class="form-group d-flex justify-content-between">
                     <div class="w-75 mr-2">
-                        <label for="branch" class="font-bold">{{__('Business')}} <span class="text-danger">*</span></label>
-                        <select id="branch" name="branch" class="w-full rounded dark:bg-dark dark:text-white my-2 form-control" required>
+                        <label for="branch" class="font-bold">{{__('Business')}} <span
+                                class="text-danger">*</span></label>
+                        <select id="branch" name="branch"
+                                class="w-full rounded dark:bg-dark dark:text-white my-2 form-control" required>
                             <option value="">{{__('Select')}}</option>
                             @foreach($businesses as $business)
                                 <option value="{{$business->id}}">{{$business->name}}</option>
@@ -21,7 +23,8 @@
                         </select>
                     </div>
                     <div class="w-100 ml-2">
-                        <label for="name" class="font-bold mb-2">{{__('Account name')}} <span class="text-danger">*</span></label>
+                        <label for="name" class="font-bold mb-2">{{__('Account name')}} <span
+                                class="text-danger">*</span></label>
                         <input type="text" id="name" name="name" class="form-control form-main-input"
                                placeholder="Perzona Capital" required
                                value="{{old('name')}}">
@@ -29,14 +32,18 @@
                 </div>
                 <div class="form-group d-flex justify-content-between">
                     <div class="w-50 mr-2">
-                        <label for="account_number" class="font-bold mb-2">{{__('Account number')}} <span class="text-danger">*</span></label>
-                        <input type="text" id="account_number" name="account_number" class="form-control form-main-input"
+                        <label for="account_number" class="font-bold mb-2">{{__('Account number')}} <span
+                                class="text-danger">*</span></label>
+                        <input type="text" id="account_number" name="account_number"
+                               class="form-control form-main-input"
                                placeholder="Enter business name" required
                                value="{{old('account_number')}}">
                     </div>
                     <div class="w-50 ms-2">
-                        <label for="institutional_key" class="font-bold">{{__('Bank')}} <span class="text-danger">*</span></label>
-                        <select id="institutional_key" name="institutional_key" class="w-full rounded dark:bg-dark dark:text-white my-2 form-control">
+                        <label for="institutional_key" class="font-bold">{{__('Bank')}} <span
+                                class="text-danger">*</span></label>
+                        <select id="institutional_key" name="institutional_key"
+                                class="w-full rounded dark:bg-dark dark:text-white my-2 form-control">
                             <option value="">{{__('Select')}}</option>
                             @foreach($banks as $bank)
                                 <option value="{{$bank->institutional_key}}">{{$bank->name}}</option>
@@ -48,22 +55,28 @@
                     <div class="w-100 mb-2">
                         <label for="description" class="font-bold mb-2">{{__('Description')}} <span class="text-danger">*</span></label>
                         <textarea type="text" id="description" name="description" class="form-control form-main-input"
-                                  placeholder="Enter business name" required
-                                  value="{{old('description')}}"></textarea>
+                                  placeholder="{{__('Enter a description')}}" required
+                                  value="{{old('description')}}" rows="3"></textarea>
                     </div>
                 </div>
                 <div class="form-group d-flex justify-content-between">
                     <div class="w-1/3 pe-2">
-                        <label for="information_number" class="font-bold mb-2">{{__('Information number')}} <span class="text-danger">*</span></label>
-                        <input id="information_number" name="information_number" class="w-100 form-control" type="text">
+                        <label for="information_number" class="font-bold mb-2">{{__('Information number')}} <span
+                                class="text-danger">*</span></label>
+                        <input id="information_number" name="information_number" class="w-100 form-control"
+                               type="number" placeholder="009273">
                     </div>
                     <div class="w-1/3 px-2">
-                        <label for="branch_number" class="font-bold mb-2">{{__('Branch number')}} <span class="text-danger">*</span></label>
-                        <input id="branch_number" class="w-100 form-control" type="text" value="{{old('branch_number')}}">
+                        <label for="branch_number" class="font-bold mb-2">{{__('Branch number')}} <span
+                                class="text-danger">*</span></label>
+                        <input id="branch_number" class="w-100 form-control" type="number"
+                               value="{{old('branch_number')}}" placeholder="77384">
                     </div>
                     <div class="w-1/3 ps-2">
-                        <label for="account_clabe" class="font-bold mb-2">{{__('CLABE account')}} <span class="text-danger">*</span></label>
-                        <input id="account_clabe" name="account_clabe" class="w-100 form-control" type="text" value="{{old('account_clabe')}}">
+                        <label for="account_clabe" class="font-bold mb-2">{{__('CLABE account')}} <span
+                                class="text-danger">*</span></label>
+                        <input id="account_clabe" name="account_clabe" class="w-100 form-control" type="number"
+                               value="{{old('account_clabe')}}" placeholder="0388492764883">
                     </div>
                 </div>
 

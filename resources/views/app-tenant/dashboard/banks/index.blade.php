5a6cccae658e23214b383bfaa9ff8eec02c85818
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-02-28 13:26:28 -0600

    Corrections

--- resources/views/app-tenant/dashboard/banks/index.blade.php
+++ resources/views/app-tenant/dashboard/banks/index.blade.php
@@ -112,68 +112,6 @@
             </table>
             <hr>
 
-            <!--
-            <div class="border-bottom-1 border-gray-400 py-3">
-                <h2>Cuenta BBVA 1256 <a href="{{route('banks.edit',1)}}" class="text-gray-400 hover:text-gray-700"><i
-                            class="fas fa-edit"></i></a></h2>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Empresa:
-                    </div>
-                    <div class="flex-1">
-                        Perzona
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Número de cuenta:
-                    </div>
-                    <div class="flex-1">
-                        2361256
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Descripción:
-                    </div>
-                    <div class="flex-1">
-                        BBVA 01
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Banco:
-                    </div>
-                    <div class="flex-1">
-                        BBVA
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Número de información:
-                    </div>
-                    <div class="flex-1">
-                        -
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Número de sucursal:
-                    </div>
-                    <div class="flex-1">
-                        2369
-                    </div>
-                </div>
-                <div class="flex pt-2">
-                    <div class="flex-1 text-gray-500">
-                        Cuenta CLABE:
-                    </div>
-                    <div class="flex-1">
-                        4523698531256
-                    </div>
-                </div>
-            </div>-->
-
         </div>
 
 

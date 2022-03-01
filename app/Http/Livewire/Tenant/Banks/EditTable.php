354ffd836a669b2a9b2bacecaa8aa288f7c2bda9
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-03-01 14:14:09 -0600

    Edit and update component created successfully

--- app/Http/Livewire/Tenant/Banks/EditTable.php
+++ app/Http/Livewire/Tenant/Banks/EditTable.php
@@ -9,10 +9,23 @@ use Livewire\Component;
 
 class EditTable extends Component
 {
-    public $userId;
+    //    Variables from our form
+    public $branch, $branch_id, $name, $account_number, $institutional_key, $description, $information_number, $branch_number, $account_clabe;
 
-    public function mount($userId){
+    public function mount($userId)
+    {
+        $this->accountBank = Bank::find($userId);
         $this->userId = $userId->id;
+
+        $this->name = $userId->name;
+        $this->branch_id = $userId->branch;
+        $this->branch = $userId->business->name;
+        $this->account_number = $userId->account_number;
+        $this->institutional_key = $userId->institutional_key;
+        $this->description = $userId->description;
+        $this->information_number = $userId->information_number;
+        $this->branch_number = $userId->branch_number;
+        $this->account_clabe = $userId->account_clabe;
     }
 
     public function render()
@@ -25,5 +38,23 @@ class EditTable extends Component
         $banks = json_decode($api_responseBanks->body());
 
         return view('livewire.tenant.banks.edit-table', compact('businesses', 'banks', 'bankAccount'));
+    }
+
+    public function update()
+    {
+        $bankAccount = Bank::find($this->userId);
+
+        $bankAccount->name = $this->name;
+        $bankAccount->branch = $this->branch_id;
+        $bankAccount->account_number = $this->account_number;
+        $bankAccount->institutional_key = $this->institutional_key;
+        $bankAccount->description = $this->description;
+        $bankAccount->information_number = $this->information_number;
+        $bankAccount->branch_number = $this->branch_number;
+        $bankAccount->account_clabe = $this->account_clabe;
+
+        $bankAccount->save();
+
+        return redirect()->route('banks.index')->with('message', 'edit');
     }
 }
--- resources/views/livewire/tenant/banks/edit-table.blade.php
+++ resources/views/livewire/tenant/banks/edit-table.blade.php
@@ -1,27 +1,25 @@
 <div class="card bg-white shadow-sm rounded p-4 pb-2 max-w-6xl my-2 mx-auto dark:bg-dark dark:text-white">
 
-    <form class="form-group" action="{{route('banks.store')}}" method="POST">
-        @csrf
+    <form class="form-group" wire:submit.prevent="update">
 
         <div class="form-group d-flex justify-content-between">
             <div class="w-75 mr-2">
                 <label for="branch" class="font-bold">{{__('Business')}} <span
                         class="text-danger">*</span></label>
                 <select id="branch" name="branch"
-                        class="w-full rounded dark:bg-dark dark:text-white my-2 form-control">
-                    <option value="">{{__($bankAccount->business->name)}}</option>
+                        class="w-full rounded dark:bg-dark dark:text-white my-2 form-control"
+                        wire:model.defer="branch_id">
                     @foreach($businesses as $business)
-                        @if($bankAccount->business->name != $business->name)
-                            <option value="{{$business->id}}">{{$business->name}}</option>
-                        @endif
+                        <option value="{{$business->id}}" }}>{{$business->name}}</option>
                     @endforeach
                 </select>
+                @error('name') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="w-100 ml-2">
                 <label for="name" class="font-bold mb-2">{{__('Account name')}} <span
                         class="text-danger">*</span></label>
                 <input type="text" id="name" name="name" class="form-control form-main-input"
-                       value="{{ $bankAccount->name }}">
+                       wire:model.defer="name">
             </div>
         </div>
         <div class="form-group d-flex justify-content-between">
@@ -29,20 +27,13 @@
                 <label for="account_number" class="font-bold mb-2">{{__('Account number')}} <span
                         class="text-danger">*</span></label>
                 <input type="number" id="account_number" name="account_number"
-                       class="form-control form-main-input"
-                       placeholder="Enter business name"
-                       value="{{ $bankAccount->account_number }}">
+                       class="form-control form-main-input" wire:model.defer="account_number">
             </div>
             <div class="w-50 ms-2">
                 <label for="institutional_key" class="font-bold">{{__('Bank')}} <span
                         class="text-danger">*</span></label>
                 <select id="institutional_key" name="institutional_key"
-                        class="w-full rounded dark:bg-dark dark:text-white my-2 form-control">
-                    @foreach($banks as $bank)
-                        @if($bankAccount->institutional_key == $bank->institutional_key)
-                            <option value="{{$bank->institutional_key}}">{{$bank->name}}</option>
-                        @endif
-                    @endforeach
+                        class="w-full rounded dark:bg-dark dark:text-white my-2 form-control" wire:model.defer="institutional_key">
                     @foreach($banks as $bank)
                         <option value="{{$bank->institutional_key}}">{{$bank->name}}</option>
                     @endforeach
@@ -53,8 +44,7 @@
             <div class="w-100 mb-2">
                 <label for="description" class="font-bold mb-2">{{__('Description')}} <span class="text-danger">*</span></label>
                 <textarea type="text" id="description" name="description" class="form-control form-main-input"
-                          placeholder="{{__('Enter a description')}}"
-                          rows="1">{{ $bankAccount->description }}</textarea>
+                          rows="1" wire:model.defer="description">{{ $description }}</textarea>
             </div>
         </div>
         <div class="form-group d-flex justify-content-between">
@@ -62,19 +52,19 @@
                 <label for="information_number" class="font-bold mb-2">{{__('Information number')}} <span
                         class="text-danger">*</span></label>
                 <input id="information_number" name="information_number" class="w-100 form-control"
-                       type="number" placeholder="009273" value="{{ $bankAccount->information_number }}">
+                       type="number" wire:model.defer="information_number">
             </div>
             <div class="w-1/3 px-2">
                 <label for="branch_number" class="font-bold mb-2">{{__('Branch number')}} <span
                         class="text-danger">*</span></label>
                 <input id="branch_number" name="branch_number" class="w-100 form-control" type="number"
-                       value="{{ $bankAccount->branch_number }}" placeholder="77384">
+                       wire:model.defer="branch_number">
             </div>
             <div class="w-1/3 ps-2">
                 <label for="account_clabe" class="font-bold mb-2">{{__('CLABE account')}} <span
                         class="text-danger">*</span></label>
                 <input id="account_clabe" name="account_clabe" class="w-100 form-control" type="number"
-                       value="{{ $bankAccount->account_clabe }}" placeholder="0388492764883">
+                       wire:model.defer="account_clabe">
             </div>
         </div>
 

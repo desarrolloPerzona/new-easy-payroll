bea334fc96ea12f1a0701941cf37123bfed394db
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-03-16 12:31:12 -0600

    Removing store function to laravel controller, all passed to livewire component

--- app/Http/Controllers/Tenant/BusinessWorkdayController.php
+++ app/Http/Controllers/Tenant/BusinessWorkdayController.php
@@ -39,42 +39,7 @@ class BusinessWorkdayController extends Controller
      */
     public function store(Request $request)
     {
-
-        $request->validate([
-            'name' => 'required|unique:business_workdays'
-        ]);
-
-        $daysArray = [
-            'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'
-        ];
-
-        $workingDay = new BusinessWorkday;
-
-        $workingDay->name = $request->get('name');
-        $workingDay->workday_type = $request->get('workday_type');
-
-//        Storing each value of days array in front
-        foreach ($daysArray as $key => $day) {
-            if ($request->get($day)) {
-                $workingDay[$day] = $request->get($day);
-                $workingDay[$day . "_from"] = $request->get($day . "_from");
-                $workingDay[$day . "_to"] = $request->get($day . "_to");
-            } else {
-                $workingDay[$day] = 0;
-            }
-        }
-
-        if ($request->get('meal_time')) {
-            $workingDay->meal_time = $request->get('meal_time');
-            $workingDay->meal_time_from = $request->get('meal_time_from');
-            $workingDay->meal_time_to = $request->get('meal_time_to');
-        } else {
-            $workingDay->meal_time = 0;
-        }
-
-        $workingDay->save();
-
-        return redirect()->route('working-day-holiday.index')->with('message', 'create');
+        //
     }
 
     /**

e8929f179416dd33d37f33f4bb89f83828afb003
Eduardo Quiñonez <eduardo.grquinonez@gmail.com>
2022-03-17 14:09:54 -0600

    Taxes tables moved to its own component (Values table)

---
+++ app/View/Components/Tenant/TableValues/TaxesTables.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\View\Components\Tenant\TableValues;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\View\Component;
+
+class TaxesTables extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+
+        $currentYear = date('Y');
+        $appUrl = config('app.url');
+
+        //        All
+        $api_responseIsn = Http::withOptions(['verify' => false])->get($appUrl .'/api/isn-payroll-taxes-all');
+        $allIsn = json_decode($api_responseIsn->body());
+
+        //        Characters to remove of state names
+        $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+
+        $names = array();
+        $stateNames = array();
+
+        //        Getting state names
+        foreach ($allIsn as $value){
+            if(!in_array($value->state, $stateNames)){
+                array_push($stateNames, strtr(str_replace(' ', '', $value->state), $unwanted_array));
+            }
+        }
+
+        //        Getting state names
+        foreach ($allIsn as $value){
+            if(!in_array($value->state, $names)){
+                array_push($names,  $value->state);
+            }
+        }
+
+        //        Loop to create a response for each state in our array
+        foreach ($stateNames as $key => $value){
+            $response = Http::withOptions(['verify' => false])->get($appUrl .'/api/isn-payroll-taxes/' . $currentYear . '/' . $names[$key]);
+            ${"array$value"} = json_decode($response->body());
+
+        }
+
+        //        Getting variables names to show them in view
+        $stateVariables = array();
+
+        foreach ($stateNames as $name){
+            array_push($stateVariables, ('array'. $name));
+        }
+
+        return view('components.tenant.table-values.taxes-tables', compact(
+            [
+                // ISN Variables
+                'stateVariables', 'stateNames', 'names'
+            ],[
+                $stateVariables
+            ]
+        ));
+    }
+}
--- resources/views/app-tenant/dashboard/table-value/index.blade.php
+++ resources/views/app-tenant/dashboard/table-value/index.blade.php
@@ -24,61 +24,7 @@
         <x-tenant.table-values.subsidies-tables />
 
         {{--        Taxes tables--}}
-        <div class="card bg-white shadow-sm rounded p-4 max-w-6xl my-3 mx-auto dark:bg-dark dark:text-white">
-            <div class="flex">
-                <div class="flex-1">
-                    <h2 class="pb-3">Tablas de impuesto sobre nómina</h2>
-                </div>
-                <div class="flex-2">
-                    <div id="retentions-button" onclick="arrowClick('isn')">
-                        <i id="isn-icon" class="fas fa-chevron-circle-down cursor-pointer"></i>
-                    </div>
-                </div>
-            </div>
-
-            <div id="collapse-isn" class="display-none mt-5" x-data="data()">
-
-                {{--                All tables for each state--}}
-                <div id="daily-table" class="retentions-table">
-
-                    @foreach($stateVariables as $key =>$name)
-                        <div>
-                            <h2>{{ $names[$key] }}</h2>
-                        </div>
-                        <hr>
-
-                        <table class="table table-striped mb-5">
-                            <thead>
-                            <tr>
-                                <th scope="col">{{__('Límite inferior $')}}</th>
-                                <th scope="col">{{__('Límite superior $')}}</th>
-                                <th scope="col">{{__('Cuota fija $')}}</th>
-                                <th scope="col">{{__('Excedentes %')}}</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach(${"$name"} as $stateValue)
-                                <tr>
-                                    <td>{{number_format($stateValue->lower_limit, 2, '.')}}</td>
-                                    @if($stateValue->upper_limit == 999999.00)
-
-                                        <td>{{__('En adelante')}}</td>
-                                    @else
-                                        <td>{{number_format($stateValue->upper_limit, 2, '.')}}</td>
-                                    @endif
-                                    <td>{{number_format($stateValue->fixed_fee, 2, '.')}}</td>
-                                    <td>{{$stateValue->lower_limit_surplus_percentage}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    @endforeach
-
-                </div>
-
-            </div>
-
-        </div>
+        <x-tenant.table-values.taxes-tables />
 
     </div>
 

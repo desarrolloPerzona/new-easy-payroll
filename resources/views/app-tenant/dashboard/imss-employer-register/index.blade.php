566671c56aad5806c621085748c288a33c084130
Eduardo Quiñonez <eduardo.grquinonez@gmail.com>
2022-03-23 14:05:27 -0600

    Create button component created

--- resources/views/app-tenant/dashboard/imss-employer-register/index.blade.php
+++ resources/views/app-tenant/dashboard/imss-employer-register/index.blade.php
@@ -3,7 +3,11 @@
 
         <x-utilities.header-section title="Employer register" classes="mb-3" maxSize=""/>
 
+        {{--Create Button--}}
+        <x-utilities.create-button title="New employer register" route="imss-employer-registers.create"/>
+
         <livewire:tenant.imms-patronal-register.show-table/>
+
     </div>
 </x-app-tenant>
 
---
+++ resources/views/components/utilities/create-button.blade.php
@@ -0,0 +1,6 @@
+<div class="btn-top-holder my-3 flex-1">
+    <a href="{{route($route)}}" class="btn btn-dark">
+        <i class="fas fa-plus-circle"></i>
+        {{ __($title) }}
+    </a>
+</div>

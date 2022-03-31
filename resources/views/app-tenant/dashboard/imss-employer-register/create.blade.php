ac9f56831ca3012fca8ec9735762e9aaf4b0c007
GIT DSSD - NUEVECUBICA <admin@c9sd.com>
2022-03-31 15:31:19 -0600

    220222 alex: chore lalo -> make components

--- resources/views/app-tenant/dashboard/imss-employer-register/create.blade.php
+++ resources/views/app-tenant/dashboard/imss-employer-register/create.blade.php
@@ -1,8 +1,6 @@
 <x-app-tenant>
     <div class="container mx-auto">
-
         <x-utilities.header-section title="IMSS Employer Registers" classes="mb-3" maxSize=""/>
-        <livewire:tenant.imss-patronal-register.create />
-
+        <livewire:tenant.dashboard.imss-patronal-register.imss-patronal-register-create />
     </div>
 </x-app-tenant>

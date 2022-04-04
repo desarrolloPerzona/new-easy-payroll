02c51616a5d73d2fa60fa4c1d993c79a72f0e642
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-04-04 12:57:50 -0500

    Refactoring

--- app/Http/Livewire/Tenant/Dashboard/ImssPatronalRegister/ImssPatronalRegisterCreate.php
+++ app/Http/Livewire/Tenant/Dashboard/ImssPatronalRegister/ImssPatronalRegisterCreate.php
@@ -51,12 +51,15 @@ class ImssPatronalRegisterCreate extends Component
 
 
 
-        if ($this->use_imss == 1) {
-            //$register->cert_imss_cert = $this->cert_imss_cert->store('certificates');
+        if ($this->use_cert == 'imss') {
+            $this->validate([
+                'cert_imss_cert' => 'required',
+            ]);
             $register->cert_imss_user = $this->cert_imss_user;
             $register->cert_imss_password = $this->cert_imss_password;
+        } else if($this->use_cert == 'fiel'){
+            dd('Entrando a fiel');
         }
-
 
         $register->save();
 

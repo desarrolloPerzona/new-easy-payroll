9c1dde97c479d9ec9e7538a6ef1e2555f05a828e
Eduardogrq <eduardo.grquinonez@gmail.com>
2022-04-04 18:13:36 -0500

    Creating show method in branches

--- resources/views/livewire/tenant/branches/show-branch.blade.php
+++ resources/views/livewire/tenant/branches/show-branch.blade.php
@@ -5,7 +5,7 @@
         <tr class="colspan-2">
             <div class="d-flex justify-between">
                 <h2 class="mb-4 ps-2"><b>{{$branch->name}}</b>
-                    <a href="{{route('branches.edit', 1)}}"
+                    <a href="{{route('branches.edit', $branch->id)}}"
                        class="text-gray-400 hover:text-gray-700 ms-2">
                         <i class="fas fa-edit"></i>
                     </a>
@@ -20,20 +20,32 @@
         </thead>
         <tbody>
         <tr>
-            <td class="pb-2 col-6"><b>ID:</b></td>
-            <td class="col-6"></td>
-        </tr>
-        <tr>
             <td class="pb-2 col-6"><b>{{__('Name')}}:</b></td>
-            <td class="col-6"></td>
+            <td class="col-6">{{$branch->name}}</td>
         </tr>
         <tr>
-            <td class="pb-2 col-6"><b>{{__('risk premium')}}:</b></td>
-            <td class="col-6"></td>
+            <td class="pb-2 col-6"><b>{{__('Description')}}:</b></td>
+            <td class="col-6">{{$branch->description}}</td>
         </tr>
         <tr>
-            <td class="pb-2 col-6"><b>{{__('IMSS subdelegational key')}}:</b></td>
-            <td class="col-6"></td>
+            <td class="pb-2 col-6"><b>{{__('Street')}}:</b></td>
+            <td class="col-6">{{$branch->street}}</td>
+        </tr>
+        <tr>
+            <td class="pb-2 col-6"><b>{{__('ZIP Code')}}:</b></td>
+            <td class="col-6">{{$branch->zip_code}}</td>
+        </tr>
+        <tr>
+            <td class="pb-2 col-6"><b>{{__('Borough')}}:</b></td>
+            <td class="col-6">{{$branch->borough}}</td>
+        </tr>
+        <tr>
+            <td class="pb-2 col-6"><b>{{__('Municipality')}}:</b></td>
+            <td class="col-6">{{$branch->municipality}}</td>
+        </tr>
+        <tr>
+            <td class="pb-2 col-6"><b>{{__('State')}}:</b></td>
+            <td class="col-6">{{$branch->state}}</td>
         </tr>
         </tbody>
     </table>

0e6092dc0b141316950ec4b993ee071be0d377ca
DSSD-NUEVE <git-dssd-nueve3@nuevecubica.net>
2022-04-06 10:37:05 -0500

    040322 Alex macbook pro chore: test route created to be copied...

--- database/migrations/tenant/2022_01_10_133604_create_imss_patronal_registers_table.php
+++ database/migrations/tenant/2022_01_10_133604_create_imss_patronal_registers_table.php
@@ -18,8 +18,8 @@ class CreateImssPatronalRegistersTable extends Migration
             $table->string('name')->unique();
             $table->decimal('risk_premium')->nullable();
             $table->string('imss_sub_delegation_key')->nullable()->unique();
-            $table->string('cert_imss_user')->unique()->nullable();
-            $table->string('cert_imss_password')->unique()->nullable();
+            $table->string('cert_imss_user')->nullable();
+            $table->string('cert_imss_password')->nullable();
             $table->string('cert_imss_cert')->unique()->nullable();
             $table->boolean('use_fiel')->default(false);
             $table->timestamps();
